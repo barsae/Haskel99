af95eeb0554a7fbc041319a545957930a808871a
ebarsalou <barsae@gmail.com>
2014-07-05 22:27:16 -0700

    Refactor: Use dropWhile instead of reimplementing

--- 10.hs
+++ 10.hs
@@ -1,14 +1,8 @@
 
-skipWhile' :: (a -> Bool) -> [a] -> [a]
-skipWhile' pred [] = []
-skipWhile' pred (x:xs)
-    | pred x = skipWhile' pred xs
-    | otherwise = (x:xs)
-
 pack :: (Eq a) => [a] -> [[a]]
 pack [] = []
 pack (x:xs) = let front = takeWhile (== x) (x:xs)
-                  back  = skipWhile' (== x) (x:xs)
+                  back  = dropWhile (== x) (x:xs)
                in front : (pack back)
 
 encode :: (Eq a) => [a] -> [(Int, a)]
