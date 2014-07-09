cd81530621d2602c794d8ca04545d06f276d8506
ebarsalou <ebarsalou@nationalresearch.com>
2014-07-09 15:55:22 -0700

    New: Solve 11

---
+++ 11.hs
@@ -0,0 +1,20 @@
+
+($$) = flip ($)
+
+data Encoded a = Multiple Int a | Single a deriving Show
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = let front = takeWhile (== x) (x:xs)
+                  back  = dropWhile (== x) (x:xs)
+               in front : (pack back)
+
+replaceSingleton :: Encoded a => Encoded a
+replaceSingleton (Multiple 1 a) = Single a
+replaceSingleton a = a
+
+encode :: (Eq a) => [a] -> [Encoded a]
+encode list = let packed = pack list
+              in zipWith Multiple (map length packed) (map head packed)
+              $$ map replaceSingleton
+ 
