a047951f005d113a949c099c86932bd75d2128fa
ebarsalou <ebarsalou@nationalresearch.com>
2014-07-09 15:33:42 -0700

    Fix: Cleanup unused testing code

--- 02.hs
+++ 02.hs
@@ -1,16 +1,7 @@
-import Control.Exception
-import Control.Monad
 
 myButLast :: [a] -> a
 myButLast [] = error "Can't call myLast on empty list"
 myButLast (x:[]) = error "Can't call myLast on single element list"
 myButLast (x:y:[]) = x
 myButLast (x:xs) = myButLast xs
-
-main :: IO ()
-main = do
-    assertException (evaluate $ myButLast [])
-    assertException (evaluate $ myButLast [1])
-    assert (myButLast [1, 2, 3]) 2
-    assert (myButLast [1, 2, 3, 4]) 3
 
